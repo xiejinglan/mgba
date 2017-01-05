ad3d35d0f34920eb66ce778afe482cfa5d59289a
sergiobenrocha2 <sergiobenrocha2@gmail.com>
2017-01-04 22:45:04 -0200

    Backport GB saving fix.

--- src/platform/libretro/libretro.c
+++ src/platform/libretro/libretro.c
@@ -607,19 +607,28 @@ void* retro_get_memory_data(unsigned id) {
 
 size_t retro_get_memory_size(unsigned id) {
 	if (id == RETRO_MEMORY_SAVE_RAM) {
-		switch (((struct GBA*) core->board)->memory.savedata.type) {
-		case SAVEDATA_AUTODETECT:
-		case SAVEDATA_FLASH1M:
-			return SIZE_CART_FLASH1M;
-		case SAVEDATA_FLASH512:
-			return SIZE_CART_FLASH512;
-		case SAVEDATA_EEPROM:
-			return SIZE_CART_EEPROM;
-		case SAVEDATA_SRAM:
-			return SIZE_CART_SRAM;
-		case SAVEDATA_FORCE_NONE:
-			return 0;
+#ifdef M_CORE_GBA
+		if (core->platform(core) == PLATFORM_GBA) {
+			switch (((struct GBA*) core->board)->memory.savedata.type) {
+			case SAVEDATA_AUTODETECT:
+			case SAVEDATA_FLASH1M:
+				return SIZE_CART_FLASH1M;
+			case SAVEDATA_FLASH512:
+				return SIZE_CART_FLASH512;
+			case SAVEDATA_EEPROM:
+				return SIZE_CART_EEPROM;
+			case SAVEDATA_SRAM:
+				return SIZE_CART_SRAM;
+			case SAVEDATA_FORCE_NONE:
+				return 0;
+			}
 		}
+#endif
+#ifdef M_CORE_GB
+		if (core->platform(core) == PLATFORM_GB) {
+			return ((struct GB*) core->board)->sramSize;
+		}
+#endif
 	}
 	if (id == RETRO_MEMORY_SYSTEM_RAM) {
 		return SIZE_WORKING_RAM;
